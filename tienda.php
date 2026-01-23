2bd3c669324887f26689d2d5772f9f3ad9f7607c
santiago <llanessantiago88@gmail.com>
2026-01-22 20:19:14 -0500

    Actualizo contenido de la web

--- tienda.php
+++ tienda.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['user'])) {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<title>Tienda | ThunderLand</title>
+<title>Tienda | Hardcore Adventure</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
 <style>
